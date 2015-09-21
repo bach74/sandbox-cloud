e2e57645ff2b6bf5ca2953c6c08891ca7f8a9bc9
developer bach <developer.bach@google.com>
2015-09-21 23:32:24 +0200

    worcount

--- mp2/TitleCount.java
+++ mp2/TitleCount.java
@@ -24,74 +24,84 @@ import java.util.StringTokenizer;
 
 // >>> Don't Change
 public class TitleCount extends Configured implements Tool {
-    public static void main(String[] args) throws Exception {
-        int res = ToolRunner.run(new Configuration(), new TitleCount(), args);
-        System.exit(res);
-    }
+	public static void main(String[] args) throws Exception {
+		int res = ToolRunner.run(new Configuration(), new TitleCount(), args);
+		System.exit(res);
+	}
 
-    @Override
-    public int run(String[] args) throws Exception {
-        Job job = Job.getInstance(this.getConf(), "Title Count");
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+	@Override
+	public int run(String[] args) throws Exception {
+		Job job = Job.getInstance(this.getConf(), "Title Count");
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
 
-        job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(IntWritable.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(IntWritable.class);
 
-        job.setMapperClass(TitleCountMap.class);
-        job.setReducerClass(TitleCountReduce.class);
+		job.setMapperClass(TitleCountMap.class);
+		job.setReducerClass(TitleCountReduce.class);
 
-        FileInputFormat.setInputPaths(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-        job.setJarByClass(TitleCount.class);
-        return job.waitForCompletion(true) ? 0 : 1;
-    }
+		job.setJarByClass(TitleCount.class);
+		return job.waitForCompletion(true) ? 0 : 1;
+	}
 
-    public static String readHDFSFile(String path, Configuration conf) throws IOException{
-        Path pt=new Path(path);
-        FileSystem fs = FileSystem.get(pt.toUri(), conf);
-        FSDataInputStream file = fs.open(pt);
-        BufferedReader buffIn=new BufferedReader(new InputStreamReader(file));
+	public static String readHDFSFile(String path, Configuration conf) throws IOException {
+		Path pt = new Path(path);
+		FileSystem fs = FileSystem.get(pt.toUri(), conf);
+		FSDataInputStream file = fs.open(pt);
+		BufferedReader buffIn = new BufferedReader(new InputStreamReader(file));
 
-        StringBuilder everything = new StringBuilder();
-        String line;
-        while( (line = buffIn.readLine()) != null) {
-            everything.append(line);
-            everything.append("\n");
-        }
-        return everything.toString();
-    }
+		StringBuilder everything = new StringBuilder();
+		String line;
+		while ((line = buffIn.readLine()) != null) {
+			everything.append(line);
+			everything.append("\n");
+		}
+		return everything.toString();
+	}
 
-// <<< Don't Change
+	// <<< Don't Change
 
-    public static class TitleCountMap extends Mapper<Object, Text, Text, IntWritable> {
-        List<String> stopWords;
-        String delimiters;
+	public static class TitleCountMap extends Mapper<Object, Text, Text, IntWritable> {
+		List<String> stopWords;
+		String delimiters;
 
-        @Override
-        protected void setup(Context context) throws IOException,InterruptedException {
+		@Override
+		protected void setup(Context context) throws IOException, InterruptedException {
 
-            Configuration conf = context.getConfiguration();
+			Configuration conf = context.getConfiguration();
 
-            String stopWordsPath = conf.get("stopwords");
-            String delimitersPath = conf.get("delimiters");
+			String stopWordsPath = conf.get("stopwords");
+			String delimitersPath = conf.get("delimiters");
 
-            this.stopWords = Arrays.asList(readHDFSFile(stopWordsPath, conf).split("\n"));
-            this.delimiters = readHDFSFile(delimitersPath, conf);
-        }
+			this.stopWords = Arrays.asList(readHDFSFile(stopWordsPath, conf).split("\n"));
+			this.delimiters = readHDFSFile(delimitersPath, conf);
+		}
 
+		@Override
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			String line = value.toString();
+			StringTokenizer tokenizer = new StringTokenizer(line, this.delimiters);
+			while (tokenizer.hasMoreTokens()) {
+				String token = tokenizer.nextToken();
+				if (!stopWords.contains(token.trim().toLowerCase())) {
+					context.write(new Text(token), new IntWritable(1));
+				}
+			}
+		}
+	}
 
-        @Override
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            // TODO
-        }
-    }
-
-    public static class TitleCountReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
-        @Override
-        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            // TODO
-        }
-    }
+	public static class TitleCountReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+		@Override
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable value : values) {
+				sum += value.get();
+			}
+			context.write(key, new IntWritable(sum));
+		}
+	}
 }
